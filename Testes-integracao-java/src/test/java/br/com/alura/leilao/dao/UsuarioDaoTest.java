a3c43fcc69a851c7ab951731ac5476748cdb414d
Robson Coura <robson_coura@yahoo.com.br>
2022-11-04 15:28:36 -0300

    refatoracao no código na camada de teste

--- Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,8 +1,11 @@
 package br.com.alura.leilao.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
@@ -10,20 +13,42 @@ import br.com.alura.leilao.util.JPAUtil;
 
 class UsuarioDaoTest {
 
+	//Atributos da classe
 	private UsuarioDao dao;
+	private EntityManager em;
 
-	@Test
-	void testeBuscaDeUsuarioPeloUsername() {
-		EntityManager em = JPAUtil.getEntityManager();
+	//Criacao do EntityManager e do Usuariodao, sendo feita no método beforeEach antes dos teste.
+	@BeforeEach
+	public void beforeEach(){
+		this.em = JPAUtil.getEntityManager();
 		this.dao = new UsuarioDao(em);
-		
-		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
 		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
-		
+	}
+
+	/*Após a execucao de cada teste ele vai chamar o método afterEach. O teste executou com tudo,
+	 que tinha que executar faz o rollback desfaz todos os insert tudo que foi feito no banco de dado,
+	 eai o proximo teste vai iniciar com o banco zerado.*/
+	@AfterEach
+	public void	afterEach(){
+		em.getTransaction().rollback();
+	}
+	@Test
+	void deveriaEncontrarUsuarioCadastrado() {
+		Usuario usuario = criarUsuario();
 		Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
 		Assert.assertNotNull(encontrado);
 	}
 
+	@Test
+	void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+		criarUsuario();
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
+	}
+
+	//Método para criar objeto usuario, e retorna o objeto já salvo do banco de dado.
+		private Usuario criarUsuario() {
+		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+		em.persist(usuario);
+		return usuario;
+	}
 }
