87b5a1dc6e3a79411fd45575498dc159e6f85c85
Robson Coura <robson_coura@yahoo.com.br>
2022-11-04 16:16:00 -0300

    Teste para verificacao se realmente foi atualizado um leilao

--- Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
+++ Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
@@ -9,48 +9,64 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 class LeilaoDaoTest {
 
-	//Atributos da classe
-	private LeilaoDao dao;
-	private EntityManager em;
+    //Atributos da classe
+    private LeilaoDao dao;
+    private EntityManager em;
 
-	//Criacao do EntityManager e do Leilaodao, sendo feita no método beforeEach antes dos teste.
-	@BeforeEach
-	public void beforeEach(){
-		this.em = JPAUtil.getEntityManager();
-		this.dao = new LeilaoDao(em);
-		em.getTransaction().begin();
-	}
+    //Criacao do EntityManager e do Leilaodao, sendo feita no método beforeEach antes dos teste.
+    @BeforeEach
+    public void beforeEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.dao = new LeilaoDao(em);
+        em.getTransaction().begin();
+    }
 
-	/*Após a execucao de cada teste ele vai chamar o método afterEach. O teste executou com tudo,
-	 que tinha que executar faz o rollback desfaz todos os insert tudo que foi feito no banco de dado,
-	 eai o proximo teste vai iniciar com o banco zerado.*/
-	@AfterEach
-	public void	afterEach(){
-		em.getTransaction().rollback();
-	}
+    /*Após a execucao de cada teste ele vai chamar o método afterEach. O teste executou com tudo,
+     que tinha que executar faz o rollback desfaz todos os insert tudo que foi feito no banco de dado,
+     eai o proximo teste vai iniciar com o banco zerado.*/
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
-	//Teste para verificar se está salvando um leilao no banco de dados
-	@Test
-	void deveriaCadastrarUmLeilao(){
-		Usuario usuario = criarUsuario();
-		Leilao leilao = new Leilao("Notebook Dell G15", new BigDecimal("2500"), LocalDate.now(), usuario);
+    //Teste para verificar se está salvando um leilao no banco de dados
+    @Test
+    void deveriaCadastrarUmLeilao() {
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Notebook Dell G15", new BigDecimal("2500"), LocalDate.now(), usuario);
 
-		leilao = dao.salvar(leilao);
+        leilao = dao.salvar(leilao);
 
-		Leilao salvo = dao.buscarPorId(leilao.getId());
-		Assert.assertNotNull(salvo);
-	}
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        Assert.assertNotNull(salvo);
+    }
 
-	//Método para criar objeto usuario, e retorna o objeto já salvo do banco de dado.
-		private Usuario criarUsuario() {
-		Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-		em.persist(usuario);
-		return usuario;
-	}
+    @Test
+    void deveriaAtualizarUmLeilao() {
+        Usuario usuario = criarUsuario();
+        Leilao leilao = new Leilao("Notebook Dell G15", new BigDecimal("2500"), LocalDate.now(), usuario);
+
+        leilao = dao.salvar(leilao);
+
+        leilao.setNome("Notebook Acer Nitro 5");
+        leilao.setValorInicial(new BigDecimal("2000"));
+
+        leilao = dao.salvar(leilao);
+
+        Leilao salvo = dao.buscarPorId(leilao.getId());
+        Assert.assertEquals("Notebook Acer Nitro 5", salvo.getNome());
+        Assert.assertEquals(new BigDecimal("2000"), salvo.getValorInicial());
+    }
+
+    //Método para criar objeto usuario, e retorna o objeto já salvo do banco de dado.
+    private Usuario criarUsuario() {
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        em.persist(usuario);
+        return usuario;
+    }
 }
