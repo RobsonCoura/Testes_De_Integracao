1affe0a8b82b23aecff1fd328b1ae03942fe3b35
Robson Coura <robson_coura@yahoo.com.br>
2022-11-06 13:43:08 -0300

    Criação da classe UsuarioBuilder para uma organizacao melhor de codigo

--- Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
+++ Testes-integracao-java/src/test/java/br/com/alura/leilao/dao/LeilaoDaoTest.java
@@ -3,6 +3,8 @@ package br.com.alura.leilao.dao;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
+import br.com.alura.leilao.util.builder.LeilaoBuider;
+import br.com.alura.leilao.util.builder.UsuarioBuilder;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,8 +39,20 @@ class LeilaoDaoTest {
     //Teste para verificar se está salvando um leilao no banco de dados
     @Test
     void deveriaCadastrarUmLeilao() {
-        Usuario usuario = criarUsuario();
-        Leilao leilao = new Leilao("Notebook Dell G15", new BigDecimal("2500"), LocalDate.now(), usuario);
+       Usuario usuario = new  UsuarioBuilder()
+               .comNome("Fulano")
+               .comEmail("fulano@email.com")
+               .comSenha("12345678")
+               .criar();
+
+       em.persist(usuario);
+
+       Leilao leilao = new LeilaoBuider()
+               .comNome("Mochila")
+               .comValorInicial("500")
+               .comData(LocalDate.now())
+               .comUsuario(usuario)
+               .criar();
 
         leilao = dao.salvar(leilao);
 
@@ -48,8 +62,20 @@ class LeilaoDaoTest {
 
     @Test
     void deveriaAtualizarUmLeilao() {
-        Usuario usuario = criarUsuario();
-        Leilao leilao = new Leilao("Notebook Dell G15", new BigDecimal("2500"), LocalDate.now(), usuario);
+            Usuario usuario = new  UsuarioBuilder()
+                    .comNome("Fulano")
+                    .comEmail("fulano@email.com")
+                    .comSenha("12345678")
+                    .criar();
+
+            em.persist(usuario);
+
+            Leilao leilao = new LeilaoBuider()
+                    .comNome("Mochila")
+                    .comValorInicial("500")
+                    .comData(LocalDate.now())
+                    .comUsuario(usuario)
+                    .criar();
 
         leilao = dao.salvar(leilao);
 
@@ -61,12 +87,5 @@ class LeilaoDaoTest {
         Leilao salvo = dao.buscarPorId(leilao.getId());
         Assert.assertEquals("Notebook Acer Nitro 5", salvo.getNome());
         Assert.assertEquals(new BigDecimal("2000"), salvo.getValorInicial());
-    }
-
-    //Método para criar objeto usuario, e retorna o objeto já salvo do banco de dado.
-    private Usuario criarUsuario() {
-        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
-        em.persist(usuario);
-        return usuario;
     }
 }
